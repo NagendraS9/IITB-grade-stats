e3150a83ae50315dce32695e72173b2058243654
NagendraS9 <81818471+NagendraS9@users.noreply.github.com>
2023-08-04 13:21:36 +0530

    Update grading stats

--- script.js
+++ script.js
@@ -1,73 +1,103 @@
 // Function to create the ag-Grid table
 function parseIntegerValue(value) {
 	return parseInt(value, 10);
+}
+
+function getContextMenuItems(params) {
+	return [
+	  'copy',
+	  'copyWithHeaders',
+	  'paste',
+	  'separator',
+		{
+			name: 'Column Chart',
+			action: ()=>onChartMenuItemClicked(params),
+		},
+    'chartRange',
+	  'export',
+	];
   }
-  
+  function onChartMenuItemClicked(params) {
+	console.log(params)
+	params.api.createRangeChart({
+		chartType: 'groupedColumn',
+		cellRange: {
+			columns: ['course_name', 'AA', 'AB', 'Total'],
+		},
+		// other options...
+	});
+  }
+  var gridOptions = {}
+  function onFilterTextBoxChanged() {
+	gridOptions.api.setQuickFilter(
+	  document.getElementById('filter-text-box').value
+	);
+  }
+
 function createAgGridTable(data) {
 	const columnDefs = [
-		{ headerName: 'Course Name', field: 'course_name', filter: 'agTextColumnFilter', sort:"asc", enableRowGroup: true, rowGroup: true, hide: true},
-		{ headerName: 'Year', field: 'year', enableRowGroup: true},
-		{ headerName: 'Semester', field: 'semester', enableRowGroup: true},
+		{ headerName: 'Course Name', field: 'course_name', filter: 'agTextColumnFilter', sort: "asc", enableRowGroup: true, rowGroup: true, hide: true },
+		{ headerName: 'Year', field: 'year', enableRowGroup: true },
+		{ headerName: 'Semester', field: 'semester', enableRowGroup: true },
 		{ headerName: 'Title', field: 'title', filter: 'agTextColumnFilter' },
-		{ headerName: 'AA', field: 'AA', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2) },
-		{ 
-			headerName: 'AA%', 
-			chartDataType: 'series', 
+		{ headerName: 'AA', field: 'AA', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2) },
+		{
+			headerName: 'AA%',
+			chartDataType: 'series',
 			valueGetter: function (params) {
 				const aa = params.data.AA;
 				const total = params.data.Total;
-				return (total !== 0) ? (aa / total * 100)  : 0;
-			}, 
-			aggFunc: 'avg', 
-			valueFormatter : params => (parseFloat(params.value).toFixed(2)+"%"),
+				return (total !== 0) ? (aa / total * 100) : 0;
+			},
+			aggFunc: 'avg',
+			valueFormatter: params => (parseFloat(params.value).toFixed(2) + "%"),
 			hide: true,
-		} ,
-		{ headerName: 'AB', field: 'AB', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2) },
-		{ 
-			headerName: 'AB%', 
-			chartDataType: 'series', 
+		},
+		{ headerName: 'AB', field: 'AB', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2) },
+		{
+			headerName: 'AB%',
+			chartDataType: 'series',
 			valueGetter: function (params) {
 				const ab = params.data.AB;
 				const total = params.data.Total;
-				return (total !== 0) ? (ab / total * 100)  : 0;
-			}, 
-			aggFunc: 'avg', 
-			valueFormatter : params => (parseFloat(params.value).toFixed(2)+"%"),
+				return (total !== 0) ? (ab / total * 100) : 0;
+			},
+			aggFunc: 'avg',
+			valueFormatter: params => (parseFloat(params.value).toFixed(2) + "%"),
 			hide: true,
-		} ,
-		{ 
-			headerName: 'AA+AB', 
-			chartDataType: 'series', 
+		},
+		{
+			headerName: 'AA+AB',
+			chartDataType: 'series',
 			valueGetter: function (params) {
 				return params.data.AB + params.data.AA;
-			}, 
-			aggFunc: 'avg', 
-			valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), 
+			},
+			aggFunc: 'avg',
+			valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2),
 			hide: true
-		} ,
-		{ 
-			headerName: '(AA+AB)%', 
-			chartDataType: 'series', 
+		},
+		{
+			headerName: '(AA+AB)%',
+			chartDataType: 'series',
 			valueGetter: function (params) {
-				const aa_ab = params.data.AA+params.data.AB;
+				const aa_ab = params.data.AA + params.data.AB;
 				const total = params.data.Total;
-				return (total !== 0) ? (aa_ab / total * 100)  : 0;
-			}, 
-			aggFunc: 'avg', 
-			valueFormatter : params => (parseFloat(params.value).toFixed(2)+"%"),
-			hide: true,
-		} ,
-		{ headerName: 'AU', field: 'AU', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'BB', field: 'BB', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'BC', field: 'BC', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'CC', field: 'CC', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'CD', field: 'CD', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'DD', field: 'DD', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'FR', field: 'FR', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2), hide: true},
-		{ headerName: 'Total', field: 'Total', chartDataType: 'series', aggFunc: 'avg', valueFormatter : params => Number.isInteger(params.value)?params.value:parseFloat(params.value).toFixed(2)},
+				return (total !== 0) ? (aa_ab / total * 100) : 0;
+			},
+			aggFunc: 'avg',
+			valueFormatter: params => (parseFloat(params.value).toFixed(2) + "%"),
+		},
+		{ headerName: 'AU', field: 'AU', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'BB', field: 'BB', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'BC', field: 'BC', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'CC', field: 'CC', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'CD', field: 'CD', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'DD', field: 'DD', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'FR', field: 'FR', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2), hide: true },
+		{ headerName: 'Total', field: 'Total', chartDataType: 'series', aggFunc: 'avg', valueFormatter: params => Number.isInteger(params.value) ? params.value : parseFloat(params.value).toFixed(2) },
 	];
 
-	const gridOptions = {
+	gridOptions = {
 		columnDefs: columnDefs,
 		rowData: data,
 		domLayout: 'autoHeight',
@@ -77,13 +107,30 @@ function createAgGridTable(data) {
 			filter: 'agNumberColumnFilter',
 			menuTabs: ['filterMenuTab', "generalMenuTab", "columnsMenuTab"],
 		},
-		animateRows: true,
+		suppressMenuHide: true,
+		autoGroupColumnDef: {
+			cellRenderer: 'agGroupCellRenderer',
+			filter: 'agGroupColumnFilter',
+		},
+		rowSelection: 'multiple',
+		groupSelectsChildren: true,
 		rowGroupPanelShow: 'always',
 		enableCharts: true,
-    	enableRangeSelection: true,
+		rowSelection: 'multiple',
+		statusBar: {
+			statusPanels: [
+				{
+					statusPanel: 'agTotalAndFilteredRowCountComponent',
+					align: 'left',
+				}
+			],
+			popupParent: document.body,
+		},
 		popupParent: document.body,
+		enableRangeSelection: true,
+		getContextMenuItems: getContextMenuItems,
 	};
-
+	
 	// Get the grid container element
 	const gridDiv = document.querySelector('#myGrid');
 
@@ -106,7 +153,7 @@ function readFileAndCreateTable() {
 					if (!item.hasOwnProperty(grade)) {
 						item[grade] = defaultGradeValue;
 					}
-					else{
+					else {
 						item[grade] = Number(item[grade])
 					}
 				});
